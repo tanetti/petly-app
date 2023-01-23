6c759daa15852a3646f0cfb3e8ae4f156c218c0a
Volodymyr Dudka <tanetti22@gmail.com>
2023-01-23 14:49:55 +0200

    Clear template

--- src/components/Shared/Section.styled.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const Section = styled.section`
-  width: 100%;
-  padding: ${({ theme }) => theme.space[6]} 0;
-`;
--- src/components/Shared/index.js
+++
@@ -1,3 +0,0 @@
-export * from './Box';
-export * from './Section.styled';
-export * from './Container.styled';
