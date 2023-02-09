44ed7920fd3406c3a67829178fb9437f909fa8cf
Volodymyr Dudka <tanetti22@gmail.com>
2023-02-09 13:46:18 +0200

    Add delete button for single notice modal

--- src/components/SingleNoticeModal/components/BottomButtons/BottomButtons.jsx
+++ src/components/SingleNoticeModal/components/BottomButtons/BottomButtons.jsx
@@ -1,10 +1,26 @@
 import { PropTypes } from 'prop-types';
+import { useState } from 'react';
 import { useAuth } from 'hooks';
+import { useDeleteNoticeMutation } from 'redux/notices/noticesApi';
 import { FavoriteButton } from 'components/Shared';
-import { ButtonsContainer, ContactLink } from './BottomButtonsStyled';
+import {
+  ButtonsContainer,
+  ContactLink,
+  DeleteButton,
+  DeleteIcon,
+} from './BottomButtonsStyled';
+import { DeletePromptModal } from 'components/DeletePromptModal/DeletePromptModal';
 
 export const BottomButtons = ({ noticeId, owner, title, petName }) => {
   const { _id: ownerId, phone, email } = owner;
+
+  const [isDeletePromptModalOpened, setIsDeletePromptModalOpened] =
+    useState(false);
+
+  const [
+    deleteNotice,
+    { isError: isNoticeDeleteError, isLoading: isNoticeDeletting },
+  ] = useDeleteNoticeMutation();
 
   const { userId } = useAuth();
 
@@ -12,13 +28,37 @@ export const BottomButtons = ({ noticeId, owner, title, petName }) => {
     ? `tel:${phone}`
     : `mailto:${email}?subject=Petly notice - ${title} (${petName})`;
 
+  const onDeleteNotice = () => {
+    deleteNotice(noticeId);
+  };
+
   return (
     <ButtonsContainer>
-      {userId === ownerId ? null : (
+      {userId === ownerId ? (
+        <DeleteButton
+          title="Delete notice"
+          loadingPosition="end"
+          endIcon={<DeleteIcon />}
+          loading={isNoticeDeletting}
+          onClick={() => setIsDeletePromptModalOpened(true)}
+        >
+          Delete
+        </DeleteButton>
+      ) : (
         <ContactLink href={contactLinkHref}>Contact</ContactLink>
       )}
 
       <FavoriteButton noticeId={noticeId} variant="modal" />
+
+      <DeletePromptModal
+        isOpened={isDeletePromptModalOpened}
+        closeModal={() => setIsDeletePromptModalOpened(false)}
+        action={onDeleteNotice}
+        isLoading={isNoticeDeletting}
+        isError={isNoticeDeleteError}
+        whoWord={petName}
+        fromWord="notices"
+      />
     </ButtonsContainer>
   );
 };
--- src/components/SingleNoticeModal/components/BottomButtons/BottomButtonsStyled.js
+++ src/components/SingleNoticeModal/components/BottomButtons/BottomButtonsStyled.js
@@ -1,4 +1,6 @@
 import { styled } from '@mui/material/styles';
+import DeleteForeverRoundedIcon from '@mui/icons-material/DeleteForeverRounded';
+import { OutlinedButton } from 'components/Shared';
 
 export const ButtonsContainer = styled('div')`
   padding-top: 40px;
@@ -49,4 +51,52 @@ export const ContactLink = styled('a')`
   &:focus {
     background-color: ${({ theme }) => theme.palette.accent.dark};
   }
+`;
+
+export const DeleteButton = styled(OutlinedButton)`
+  width: 100%;
+  padding: 6px 28px;
+
+  font-family: inherit;
+  font-size: 16px;
+  line-height: 1.38;
+
+  color: ${({ theme }) => theme.palette.text.white};
+  background-color: ${({ theme }) => theme.palette.accent.main};
+
+  transition: ${({ theme }) =>
+    theme.transitions.create(['color', 'background-color', 'border-color'], {
+      duration: theme.transitions.duration.standard,
+    })};
+
+  ${({ theme }) => theme.breakpoints.up('tablet')} {
+    width: 180px;
+  }
+
+  &:hover,
+  &:focus {
+    color: ${({ theme }) => theme.palette.text.white};
+    background-color: ${({ theme }) => theme.palette.accent.dark};
+  }
+
+  &:not(:last-of-type) {
+    margin-bottom: 12px;
+
+    ${({ theme }) => theme.breakpoints.up('tablet')} {
+      margin-bottom: 0;
+    }
+  }
+
+  &:not(.MuiLoadingButton-loading) svg {
+    width: 22px;
+    height: 22px;
+  }
+
+  &.MuiLoadingButton-loading > div {
+    right: calc(50% - 40px);
+  }
+`;
+
+export const DeleteIcon = styled(DeleteForeverRoundedIcon)`
+  font-size: inherit;
 `;
